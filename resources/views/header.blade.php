6fb7c1c97eb9d43eddc1d79d00e92d9a40fa891c
Stepan <stepan.rulyov@gmail.com>
2024-06-11 17:58:11 +0300

    Фиксы расположения файлов

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Social Network</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
 <header>
