823437a0791456e1546b245fc6c1b20e042b7611
Stepan <stepan.rulyov@gmail.com>
2024-06-11 20:09:53 +0300

    Ребрендинг

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Social Network</title>
+    <title>Asocial Network</title>
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
 <header>
     <div>
-        <h1>Social Network</h1>
+        <h1>Asocial Network</h1>
         <nav>
             <a href="/">Main</a>
             <a href="/posts">Posts</a>
@@ -24,7 +24,7 @@
     </div>
 </main>
 <footer>
-    <p>&copy; 2024 Social Network</p>
+    <p>&copy; 2024 Asocial Network</p>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="{{ asset("js/footer.js") }}"></script>
