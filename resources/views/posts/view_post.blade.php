c1df476c4cb977628d0eaa832e81a31f5aa55a32
Stepan <stepan.rulyov@gmail.com>
2024-06-17 19:17:27 +0300

    Добавлено отображение переноса строк

--- resources/views/posts/view_post.blade.php
+++ resources/views/posts/view_post.blade.php
@@ -24,7 +24,7 @@
                 @endif
             </div>
             <div class="post-content">
-                <p>{{ $post_info->content }}</p>
+                {!! nl2br(e($post_info->content)) !!}
                 @if(!is_null($post_info->image))
                     <img src="{{ asset("storage/" . $post_info->image) }}" alt="{{ $post_info->title }}" width="500" height="auto" style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
                 @endif
