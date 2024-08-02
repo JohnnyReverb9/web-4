50f57baad697b9716631b6942ae03743f87d0a05
Stepan <stepan.rulyov@gmail.com>
2024-08-02 18:45:01 +0300

    Получения всех постов для отправки их в пдф файл

--- app/Http/Controllers/ControllerSettings.php
+++ app/Http/Controllers/ControllerSettings.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Tcpdf;
 
 class ControllerSettings extends Controller
 {
+    private string $title = "Document AsocNet";
+
     public function index()
     {
         return view("/settings/index");
@@ -24,11 +27,21 @@ class ControllerSettings extends Controller
 
     public function downloadPosts()
     {
+        $today = today()->toDateTimeString("minute");
+        $posts = $this->getPosts();
         $pdf = new Tcpdf();
         $pdf->AddPage();
-        $pdf->SetFont('helvetica', '', 12);
-        $pdf->Write(0, 'Hello World');
-        $pdf->Output('example.pdf', 'I');
+        $pdf->SetFont('helvetica', 'courier new', 12);
+        // $pdf->Write(0, 'Hello World');
+
+        foreach ($posts as $post)
+        {
+            $pdf->Write(1, $post->title);
+        }
+
+        $title = $this->title . " POSTS " . $today;
+
+        $pdf->Output($title, "I");
     }
 
     public function downloadPermanents()
@@ -39,5 +52,19 @@ class ControllerSettings extends Controller
     public function downloadTopics()
     {
 
+    }
+
+    private function getPosts()
+    {
+        $posts = Post::where("is_published", 1)->get();
+
+        return $posts;
+    }
+
+    private function getPermanents()
+    {
+        $permanents = Post::where("is_published", 0)->get();
+
+        return $permanents;
     }
 }
