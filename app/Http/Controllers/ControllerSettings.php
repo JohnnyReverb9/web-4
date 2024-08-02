88853c049d92d19cdaf00936e592c91f6131bf6f
Stepan <stepan.rulyov@gmail.com>
2024-08-02 19:54:50 +0300

    Добавлена загрузка файла с постами

--- app/Http/Controllers/ControllerSettings.php
+++ app/Http/Controllers/ControllerSettings.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Tcpdf;
+use Nette\Utils\ImageType;
 
 class ControllerSettings extends Controller
 {
@@ -25,23 +26,45 @@ class ControllerSettings extends Controller
 
     }
 
-    public function downloadPosts()
+    public function downloadPosts(): void
     {
-        $today = today()->toDateTimeString("minute");
         $posts = $this->getPosts();
+        $i = 1;
+
         $pdf = new Tcpdf();
         $pdf->AddPage();
-        $pdf->SetFont('helvetica', 'courier new', 12);
-        // $pdf->Write(0, 'Hello World');
+        $pdf->SetFont('freeserif', '', 12);
 
         foreach ($posts as $post)
         {
-            $pdf->Write(1, $post->title);
+            $pdf->SetFont('freeserif', 'B', 14);
+            $pdf->Write(0, "Post #" . $i . ": " . $post->title . "\n\n");
+
+            $pdf->SetFont('freeserif', '', 12);
+            $pdf->Write(0, $post->content . "\n\n");
+
+            if ($post->image)
+            {
+                $image_path = storage_path("app/public/" . $post->image);
+
+                if (file_exists($image_path))
+                {
+                    $type = str_ends_with($image_path, "jpg") ? "JPG" : "PNG";
+                    $pdf->Image($image_path, 75, $pdf->GetY(), 60, 60, $type, '', 'T', true, 300, '', false, false, 1, false, false, false);
+                    $pdf->Ln(70);
+                }
+            }
+
+            $pdf->Line(10, $pdf->GetY(), 200, $pdf->GetY());
+            $pdf->Ln(10);
+
+            $i++;
         }
 
-        $title = $this->title . " POSTS " . $today;
+        $today = now()->format("H:i:s d-m-Y");
+        $title = $this->title . " POSTS " . $today . ".pdf";
 
-        $pdf->Output($title, "I");
+        $pdf->Output($title, "D");
     }
 
     public function downloadPermanents()
