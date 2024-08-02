62442bfa6ab93b323909b47c3a21fbef238f4d0f
Stepan <stepan.rulyov@gmail.com>
2024-08-02 20:00:54 +0300

    Добавлена загрузка файла с перманентами

--- app/Http/Controllers/ControllerSettings.php
+++ app/Http/Controllers/ControllerSettings.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Topics;
 use Illuminate\Http\Request;
 use App\Tcpdf;
 use Nette\Utils\ImageType;
@@ -67,9 +68,45 @@ class ControllerSettings extends Controller
         $pdf->Output($title, "D");
     }
 
-    public function downloadPermanents()
+    public function downloadPermanents(): void
     {
+        $permanents = $this->getPermanents();
+        $i = 1;
 
+        $pdf = new Tcpdf();
+        $pdf->AddPage();
+        $pdf->SetFont('freeserif', '', 12);
+
+        foreach ($permanents as $permanent)
+        {
+            $pdf->SetFont('freeserif', 'B', 14);
+            $pdf->Write(0, "Permanent #" . $i . ": " . $permanent->title . "\n\n");
+
+            $pdf->SetFont('freeserif', '', 12);
+            $pdf->Write(0, $permanent->content . "\n\n");
+
+            if ($permanent->image)
+            {
+                $image_path = storage_path("app/public/" . $permanent->image);
+
+                if (file_exists($image_path))
+                {
+                    $type = str_ends_with($image_path, "jpg") ? "JPG" : "PNG";
+                    $pdf->Image($image_path, 75, $pdf->GetY(), 60, 60, $type, '', 'T', true, 300, '', false, false, 1, false, false, false);
+                    $pdf->Ln(70);
+                }
+            }
+
+            $pdf->Line(10, $pdf->GetY(), 200, $pdf->GetY());
+            $pdf->Ln(10);
+
+            $i++;
+        }
+
+        $today = now()->format("H:i:s d-m-Y");
+        $title = $this->title . " PERMANENTS " . $today . ".pdf";
+
+        $pdf->Output($title, "D");
     }
 
     public function downloadTopics()
@@ -89,5 +126,12 @@ class ControllerSettings extends Controller
         $permanents = Post::where("is_published", 0)->get();
 
         return $permanents;
+    }
+
+    private function getTopics()
+    {
+        $topics = Topics::all();
+
+        return $topics;
     }
 }
